8ef1e6e39cee2ccf942f1041e231b489d426060a
Bastian Müller <bastian@turbolent.com>
2014-05-13 23:40:46 +0200

    rename main to example, add bzip2 support

--- src/example.c
+++ src/example.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <getopt.h>
 #include <zlib.h>
-
+#include <bzlib.h>
 
 #include "parser.h"
 #include "string.h"
@@ -15,20 +16,30 @@ int read_gzip(unsigned char* p, unsigned int len, void *read_arg) {
     return gzread(file, p, len);
 }
 
+int read_bzip(unsigned char* p, unsigned int len, void *read_arg) {
+    BZFILE *file = read_arg;
+    return BZ2_bzread(file, p, len);
+}
+
 void handler(entee_token_type type, string *s) {
-    // TODO: handle type, s->chars
+    printf("%d => %s\n", type, s->chars);
 }
 
 
 int main(int argc, char **argv) {
     char *filename = NULL;
 
+    bool isBzip = false;
+
     // parse options
     int c;
-    while ((c = getopt(argc, argv, "f:")) != -1) {
+    while ((c = getopt(argc, argv, "bf:")) != -1) {
         switch (c) {
         case 'f':
             filename = optarg;
+            break;
+        case 'b':
+            isBzip = true;
             break;
         default:
             return EXIT_FAILURE;
@@ -40,7 +51,9 @@ int main(int argc, char **argv) {
     }
 
     // open
-    gzFile file = gzopen(filename, "rb");
+    void *file = (isBzip
+                  ? BZ2_bzopen(filename, "rb")
+                  : gzopen(filename, "rb"));
 
     if (!file) {
         fprintf(stderr, "Unable to open file \"%s\"", filename);
@@ -50,7 +63,9 @@ int main(int argc, char **argv) {
     // parse
     entee_parser *parser = entee_new_parser();
 
-    entee_parser_set_reader(parser, read_gzip, file);
+    entee_reader reader = isBzip ? read_bzip : read_gzip;
+
+    entee_parser_set_reader(parser, reader, file);
     entee_parser_set_handler(parser, handler, NULL);
 
     entee_parser_parse(parser);
@@ -58,16 +73,20 @@ int main(int argc, char **argv) {
     entee_free_parser(parser);
 
     // close
-    if (!gzeof(file)) {
-        int err;
-        const char *error_string = gzerror(file, &err);
-        if (err) {
-            fprintf(stderr, "gzip error: %s.\n", error_string);
-            return EXIT_FAILURE;
+    if (isBzip) {
+        BZ2_bzclose(file);
+    } else {
+        if (!gzeof(file)) {
+            int err;
+            const char *error_string = gzerror(file, &err);
+            if (err) {
+                fprintf(stderr, "gzip error: %s.\n", error_string);
+                return EXIT_FAILURE;
+            }
         }
-    }
 
-    gzclose(file);
+        gzclose(file);
+    }
 
     return EXIT_SUCCESS;
 }
